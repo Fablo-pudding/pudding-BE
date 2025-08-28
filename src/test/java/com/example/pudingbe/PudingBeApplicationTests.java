a16420afdb22f353e7a05089ccc6dbab7b12a8f9
Kanghee <kh63167574@gmail.com>
2025-08-28 20:44:42 +0900

    frist

---
+++ src/test/java/com/example/pudingbe/PudingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pudingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PudingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
