bb4cc8573006be46f7f52acde70c7231d2ee69f1
Kanghee <kh63167574@gmail.com>
2025-09-06 03:27:53 +0900

    feat: 공지 어드민만쓸수있는 create 구현

--- src/main/java/com/example/pudingbe/PudingBeApplication.java
+++ src/main/java/com/example/pudingbe/PudingBeApplication.java
@@ -2,11 +2,12 @@ package com.example.pudingbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PudingBeApplication {
     public static void main(String[] args) {
         SpringApplication.run(PudingBeApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/example/pudingbe/notice/service/NoticeUserService.java
@@ -0,0 +1,25 @@
+package com.example.pudingbe.notice.service;
+
+import com.example.pudingbe.notice.domain.NoticeUser;
+import com.example.pudingbe.notice.repository.NoticeUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class NoticeUserService {
+    @Autowired
+    private NoticeUserRepository noticeUserRepository;
+    private LocalDateTime localDateTime = LocalDateTime.now();
+
+    public ResponseEntity<NoticeUser> PostNotice(NoticeUser noticeUser) {
+        noticeUser.setUpdateDatetime(localDateTime);
+        noticeUser.setRegisterDatetime(localDateTime);
+
+        this.noticeUserRepository.save(noticeUser);
+
+        return ResponseEntity.ok().build();
+    }
+}
