4bf0b6175cf33b540790292319c18e11a4c99dbc
Kanghee <kh63167574@gmail.com>
2025-09-06 23:01:50 +0900

    feat: 공지 UPDATE
    공지 수정기능추가완료

--- src/main/java/com/example/pudingbe/notice/service/NoticeUserService.java
+++ src/main/java/com/example/pudingbe/notice/service/NoticeUserService.java
@@ -2,6 +2,7 @@ package com.example.pudingbe.notice.service;
 
 import com.example.pudingbe.notice.domain.NoticeUser;
 import com.example.pudingbe.notice.repository.NoticeUserRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,29 @@ public class NoticeUserService {
     }
     public List<NoticeUser> GetNotices() {
         return this.noticeUserRepository.findAll();
+    }
+
+
+    public ResponseEntity<NoticeUser> PutNoticeById(NoticeUser noticeUser, long id) {
+        Optional<NoticeUser> optNoticeUser = this.noticeUserRepository.findById(id);
+    if(optNoticeUser.isEmpty()) {
+        System.out.println("[Error:PutNoticeId] There is no data of id (" + id + ")");
+        TODO:
+            return ResponseEntity.notFound().build();
+    }
+    else{
+        NoticeUser afterNoticeUser = optNoticeUser.get();
+        System.out.println("afterNoticeUser: " + afterNoticeUser.toString());
+
+        afterNoticeUser.setUpdateDatetime(localDateTime);
+        afterNoticeUser.setUpdateEmployeeNo(noticeUser.getUpdateEmployeeNo());
+
+        afterNoticeUser.setTitle(noticeUser.getTitle());
+        afterNoticeUser.setContent(noticeUser.getContent());
+
+        this.noticeUserRepository.save(afterNoticeUser);
+    }
+    return ResponseEntity.ok().build();
     }
 
     public ResponseEntity<NoticeUser> DeleteNotice(Long id) {
