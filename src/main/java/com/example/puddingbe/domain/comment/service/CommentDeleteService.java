392e8281d0a0895e6bd607d7bdc1f2c4d2529083
rudwk <tlsruwk12345@gmail.com>
2025-09-18 01:35:49 +0900

    feat : delete 기능 추가

---
+++ src/main/java/com/example/puddingbe/domain/comment/service/CommentDeleteService.java
@@ -0,0 +1,24 @@
+package com.example.puddingbe.domain.comment.service;
+
+import com.example.puddingbe.domain.comment.entity.Comment;
+import com.example.puddingbe.domain.comment.entity.repository.CommentRepository;
+import com.example.puddingbe.domain.feed.entity.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentDeleteService {
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+
+    public void Delete(Long commentId, Long userId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("댓글이 존재하지 않음"));
+
+        if(!comment.getUser_id().equals(userId)) {
+            throw new IllegalArgumentException("권한없음");
+        }
+
+        commentRepository.delete(comment);
+    }
+}
