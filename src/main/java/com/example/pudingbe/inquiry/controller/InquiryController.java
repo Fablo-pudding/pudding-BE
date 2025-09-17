60cc10f22262b53d87ffa5b8575b6298de7648dc
김지윤 <dpdupplexx9@gmail.com>
2025-09-18 00:39:41 +0900

    refactor : 무의미 공백, 리턴값 제거 및 상태코드 어노테이션 처리

--- src/main/java/com/example/pudingbe/inquiry/controller/InquiryController.java
+++ src/main/java/com/example/pudingbe/inquiry/controller/InquiryController.java
@@ -4,6 +4,7 @@ import com.example.pudingbe.inquiry.domain.dto.InquiryRequest;
 import com.example.pudingbe.inquiry.domain.dto.InquiryResponse;
 import com.example.pudingbe.inquiry.service.InquiryCreateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,8 @@ public class InquiryController {
     private final InquiryCreateService inquiryCreateService;
 
     @PostMapping
-    public ResponseEntity<InquiryResponse> createInquiry(@RequestBody InquiryRequest request) {
-        return ResponseEntity.ok(inquiryCreateService.createInquiry((request)));
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createInquiry(@RequestBody InquiryRequest request) {
+        inquiryCreateService.createInquiry(request);
     }
 }
