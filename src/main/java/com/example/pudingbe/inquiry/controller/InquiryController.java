63b48fe968cc2633364dd335bd5e91bfbb4c94c2
김지윤 <dpdupplexx9@gmail.com>
2025-09-18 14:40:27 +0900

    refactor : 상태코드 어노테이션 추가

--- src/main/java/com/example/pudingbe/inquiry/controller/InquiryController.java
+++ src/main/java/com/example/pudingbe/inquiry/controller/InquiryController.java
@@ -3,6 +3,7 @@ package com.example.pudingbe.inquiry.controller;
 import com.example.pudingbe.inquiry.domain.dto.InquiryResponse;
 import com.example.pudingbe.inquiry.service.InquiryReadService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -14,20 +15,23 @@ public class InquiryController {
     private final InquiryReadService inquiryReadService;
 
     @GetMapping
-    public ResponseEntity<List<InquiryResponse>> getAllInquiries() {
+    @ResponseStatus(HttpStatus.OK)
+    public List<InquiryResponse> getAllInquiries() {
         //TODO: ADMIN 권한 체크
-        return ResponseEntity.ok(inquiryReadService.getAllInquiries());
+        return inquiryReadService.getAllInquiries();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<InquiryResponse> getInquiryById(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public InquiryResponse getInquiryById(@PathVariable Long id) {
         //TODO: ADMIN, 작성자 권한 체크
-        return ResponseEntity.ok(inquiryReadService.getInquiryById(id));
+        return inquiryReadService.getInquiryById(id);
     }
 
     @GetMapping("/my/{userId}")
-    public ResponseEntity<List<InquiryResponse>> getMyInquiries(@PathVariable Long userId) {
+    @ResponseStatus(HttpStatus.OK)
+    public List<InquiryResponse> getMyInquiries(@PathVariable Long userId) {
         //TODO: 작성자 권한 체크
-        return ResponseEntity.ok(inquiryReadService.getMyInquiries(userId));
+        return inquiryReadService.getMyInquiries(userId);
     }
 }
