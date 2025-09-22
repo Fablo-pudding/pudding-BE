58682ae591a5c62aef3b5b139eb2c732c91070e6
rudwk <tlsruwk12345@gmail.com>
2025-09-23 01:49:21 +0900

    refactor : 파일 제거
    inquiry, PudingBeApplication

--- src/main/java/com/example/pudingbe/inquiry/domain/repository/InquiryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.pudingbe.inquiry.domain.repository;
-
-import com.example.pudingbe.inquiry.domain.entity.Inquiry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
-
-}
