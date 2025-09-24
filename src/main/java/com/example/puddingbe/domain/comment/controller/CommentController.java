6e0dad40e8585f4bafc7e63144be65b2506a0da9
rudwk <tlsruwk12345@gmail.com>
2025-09-25 00:37:25 +0900

    refactor : controller 스테이터스 코드 반환 개선

--- src/main/java/com/example/puddingbe/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/puddingbe/domain/comment/controller/CommentController.java
@@ -22,22 +22,22 @@ public class CommentController {
 
     // 댓글 추가
     @PostMapping("/create/{post-id}")
-    public ResponseEntity<Void> createComment(@PathVariable("post-id") Long postId, @RequestBody @Validated CommentRequestDTO dto) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createComment(@PathVariable("post-id") Long postId, @RequestBody @Validated CommentRequestDTO dto) {
         commentCreateService.create(postId, dto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     // 댓글 수정
     @PatchMapping("/update/{comment-id}")
-    public ResponseEntity<Void> updateComment(@PathVariable("comment-id") Long commentId, @RequestBody @Validated CommentRequestDTO dto) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateComment(@PathVariable("comment-id") Long commentId, @RequestBody @Validated CommentRequestDTO dto) {
         commentUpdateService.update(commentId, dto);
-        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     // 댓글 삭제 - 일단 임시로 path로 userId가져옴
     @DeleteMapping("/delete/{comment-id}/{user-id}")
-    public ResponseEntity<Void> deleteComment(@PathVariable("comment-id") Long commentId, @PathVariable("user-id") Long userId) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComment(@PathVariable("comment-id") Long commentId, @PathVariable("user-id") Long userId) {
         commentDeleteService.Delete(commentId, userId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
