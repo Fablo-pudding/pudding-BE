2389041fd92a481157bccf021b40b31f0d5f7c27
eeuunn <haspring@dsm.hs.kr>
2025-09-25 22:07:46 +0900

    .

--- src/main/java/com/example/puddingbe/global/config/SecurityConfig.java
+++ src/main/java/com/example/puddingbe/global/config/SecurityConfig.java
@@ -39,24 +39,4 @@ public class SecurityConfig {
                 .anyRequest().permitAll());
     }
 }
-package com.example.puddingbe.global.config;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-@Configuration
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf ->csrf.disable())
-                .authorizeHttpRequests(auth ->auth
-                        .requestMatchers(HttpMethod.DELETE, "/inquiry/{inquiry-id}").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/inquiry").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .httpBasic(httpBasic-> httpBasic.disable());
-        return http.build();
-    }
-}
+
