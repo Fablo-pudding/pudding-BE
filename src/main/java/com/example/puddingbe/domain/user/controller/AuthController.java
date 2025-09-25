63596230ab706500d85140928f2a8dad5a587ae0
eeuunn <haspring@dsm.hs.kr>
2025-09-25 21:18:03 +0900

    .

--- src/main/java/com/example/puddingbe/domain/user/controller/AuthController.java
+++ src/main/java/com/example/puddingbe/domain/user/controller/AuthController.java
@@ -5,10 +5,10 @@ import com.example.puddingbe.domain.user.service.SignupService;
 import com.example.puddingbe.global.jwt.JwtToken;
 import com.example.puddingbe.domain.user.request.LoginRequest;
 import com.example.puddingbe.domain.user.service.LoginService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,13 +20,13 @@ public class AuthController {
     private final LoginService loginService;
 
     @PostMapping("/signup")
-    public ResponseEntity<HttpStatus> signup(@Valid @RequestBody SignUpRequest signUpRequest) { // Valid를 추가해야 dto의 조건들 적용가능
+    public ResponseEntity<HttpStatus> signup(@Validated @RequestBody SignUpRequest signUpRequest) { // Valid를 추가해야 dto의 조건들 적용가능
         signupService.save(signUpRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtToken> login(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<JwtToken> login(@Validated @RequestBody LoginRequest loginRequest) {
         JwtToken jwtToken = loginService.login(loginRequest.getName(), loginRequest.getPassword());
         return ResponseEntity.ok(jwtToken); // jwt 반환
     }
