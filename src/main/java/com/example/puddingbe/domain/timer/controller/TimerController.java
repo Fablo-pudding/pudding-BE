bd3cc2f9e4c343a6c47080620bf287751dd2a6b7
iokshoi <iokshoi0915@dsm.hs.kr>
2025-09-29 22:17:57 +0900

    refactor
    TimerService -> TimerCreateService 로 클래스명 수정

--- src/main/java/com/example/puddingbe/domain/timer/controller/TimerController.java
+++ src/main/java/com/example/puddingbe/domain/timer/controller/TimerController.java
@@ -1,6 +1,6 @@
 package com.example.puddingbe.domain.timer.controller;
 
-import com.example.puddingbe.domain.timer.sevice.TimerService;
+import com.example.puddingbe.domain.timer.sevice.TimerCreateService;
 import com.example.puddingbe.domain.timer.domain.dto.TimerRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor // final 필드에 대한 생성자 자동 생성
 @RequestMapping("/timer")
 public class TimerController {
-    private final TimerService timerService;
+    private final TimerCreateService timerCreateService;
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public void createTimer(@RequestBody TimerRequest timerRequest) {
-        timerService.createTimer(timerRequest);
+        timerCreateService.createTimer(timerRequest);
     }
 }
