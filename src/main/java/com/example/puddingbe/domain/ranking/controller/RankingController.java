5867cfd0f57aa7545f11346c0ae6123e90f909dc
김지윤 <dpdupplexx9@gmail.com>
2025-09-30 00:11:26 +0900

    feat : 랭킹 controller, entity, repository 추가

--- src/main/java/com/example/puddingbe/domain/ranking/controller/RankingController.java
+++ src/main/java/com/example/puddingbe/domain/ranking/controller/RankingController.java
@@ -1,8 +1,7 @@
 package com.example.puddingbe.domain.ranking.controller;
 
-import com.example.puddingbe.domain.ranking.domain.dto.RankingResponse;
 import com.example.puddingbe.domain.ranking.domain.entity.Ranking;
-import com.example.puddingbe.domain.ranking.service.RankingReadService;
+import com.example.puddingbe.domain.ranking.repository.RankingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +11,18 @@ import java.util.List;
 @RequestMapping("/ranking")
 @RequiredArgsConstructor
 public class RankingController {
-    private RankingReadService rankingReadService;
+    private RankingRepository rankingRepository;
 
     @GetMapping("/global")
     @ResponseStatus(HttpStatus.OK)
-    public List<RankingResponse> getAllRanking() {
-        return rankingReadService.getAllRanking();
+    public List<Ranking> getAllRankng() {
+        return rankingRepository.findAllByOrderByTotalPuddingDesc();
     }
 
     @GetMapping("/{user-id}")
     @ResponseStatus(HttpStatus.OK)
     public Ranking getUserRanking(@PathVariable Long userId) {
-        return rankingReadService.getUserRanking();
+        return rankingRepository.findByUserId(userId)
+                .orElseThrow(()->new RuntimeException("유저를 찾을 수 없습니다."));
     }
 }
