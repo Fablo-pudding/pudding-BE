1031ad3dfbb89f5627c96d94fcb75b35d7815739
김지윤 <dpdupplexx9@gmail.com>
2025-09-30 00:11:26 +0900

    feat : 랭킹 controller, entity, repository 추가

--- src/main/java/com/example/puddingbe/domain/ranking/repository/RankingRepository.java
+++ src/main/java/com/example/puddingbe/domain/ranking/repository/RankingRepository.java
@@ -1,9 +1,4 @@
 package com.example.puddingbe.domain.ranking.repository;
 
-import com.example.puddingbe.domain.ranking.domain.entity.Ranking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RankingRepository extends JpaRepository<Ranking, Long> {
-    List<Ranking> findByUserId(Long userId);
+public class RankingRepository {
 }
