d77a7d633cf7b1a4441dfaf1d3d108b0d4706f92
김지윤 <dpdupplexx9@gmail.com>
2025-10-02 21:46:02 +0900

    refactor : 어드민, 작성자 본인 권한 체크 추가

--- src/main/java/com/example/puddingbe/domain/inquiry/service/InquiryDeleteService.java
+++ src/main/java/com/example/puddingbe/domain/inquiry/service/InquiryDeleteService.java
@@ -2,6 +2,7 @@ package com.example.puddingbe.domain.inquiry.service;
 
 import com.example.puddingbe.domain.inquiry.domain.entity.Inquiry;
 import com.example.puddingbe.domain.inquiry.repository.InquiryRepository;
+import com.example.puddingbe.global.detail.UserDetail;
 import com.example.puddingbe.global.exception.InquiryNotFoundException;
 import com.example.puddingbe.global.exception.UnauthorizedException;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +13,13 @@ import org.springframework.stereotype.Service;
 public class InquiryDeleteService {
     private final InquiryRepository inquiryRepository;
 
-    public void deleteInquiry(Long id, Long currentUserId, boolean isAdmin) {
+    public void deleteInquiry(Long id, UserDetail userDetail) {
         Inquiry inquiry = inquiryRepository.findById(id)
                 .orElseThrow(() -> new InquiryNotFoundException(id));
-
-        if(!isAdmin && !inquiry.getUserId().equals(currentUserId)) {
-            throw new UnauthorizedException("본인 문의만 삭제할 수 있습니다.");
+        boolean isAdmin = userDetail.getAuthorities().stream()
+                        .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        if(!isAdmin && !inquiry.getUserId().equals(userDetail.getId())) {
+            throw new UnauthorizedException("작성자 본인 또는 관리자만 접근할 수 있습니다.");
         }
         inquiryRepository.deleteById(id);
     }
