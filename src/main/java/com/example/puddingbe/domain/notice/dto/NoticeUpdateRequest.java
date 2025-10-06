07b45362b108d4f008107ff22fea2d41611fb8cc
Kanghee <kh63167574@gmail.com>
2025-10-06 19:58:51 +0900

    feat: updaterequest 제작완료

--- src/main/java/com/example/puddingbe/domain/notice/dto/NoticeUpdateRequest.java
+++ src/main/java/com/example/puddingbe/domain/notice/dto/NoticeUpdateRequest.java
@@ -1,4 +1,20 @@
 package com.example.puddingbe.domain.notice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class NoticeUpdateRequest {
+    @NotBlank(message="id는 필수입니다.")
+    private long id;
+
+    @NotBlank(message="title는 필수입니다.")
+    private String title;
+
+    @NotBlank(message="content는 필수입니다.")
+    private String content;
 }
