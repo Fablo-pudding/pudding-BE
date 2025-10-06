1d827e492778dff8491766bd38c86a7f0bd1691e
Kanghee <kh63167574@gmail.com>
2025-10-06 19:55:57 +0900

    refactor: 간편화

--- src/main/java/com/example/puddingbe/domain/notice/dto/NoticeReadResponse.java
+++ src/main/java/com/example/puddingbe/domain/notice/dto/NoticeReadResponse.java
@@ -1,1 +1,33 @@
+package com.example.puddingbe.domain.notice.dto;
+
+<<<<<<< HEAD
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalDateTime;
+import com.example.puddingbe.domain.notice.domain.NoticeUser;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class NoticeReadResponse {
+
+    private long id;
+    private String title;
+    private String content;
+    private LocalDateTime registerDateTime;
+    private LocalDateTime updateDateTime;
+    private String registerEmployeeNo;
+    private String updateEmployeeNo;
+
+    public NoticeReadResponse(NoticeUser noticeUser) {
+        this.id = noticeUser.getId();
+        this.title = noticeUser.getTitle();
+        this.content = noticeUser.getContent();
+        this.registerDateTime = noticeUser.getRegisterDatetime();
+        this.updateDateTime = noticeUser.getUpdateDatetime();
+        this.registerEmployeeNo = noticeUser.getRegisterEmployeeNo();
+        this.updateEmployeeNo = noticeUser.getUpdateEmployeeNo();
+    }
 }
