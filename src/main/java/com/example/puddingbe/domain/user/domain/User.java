18849a28a1084bdeaf71975e88391d3122783131
iokshoi <iokshoi0915@dsm.hs.kr>
2025-10-09 02:28:11 +0900

    refactor
    UserEntity 비밀번호 컬럼 길이 변경

--- src/main/java/com/example/puddingbe/domain/user/domain/User.java
+++ src/main/java/com/example/puddingbe/domain/user/domain/User.java
@@ -16,7 +16,7 @@ public class User {
     @Column(nullable = false, length = 30)
     private String name;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 255)
     private String password;
 
     @Column(nullable = false)
