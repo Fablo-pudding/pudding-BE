6a20ac8bba561c923c4362915203d42ff44e3bb8
rudwk <tlsruwk12345@gmail.com>
2025-10-23 23:10:16 +0900

    refactor : 파일 구조 정리

--- src/main/java/com/example/puddingbe/domain/comment/service/CommentUpdateService.java
+++ src/main/java/com/example/puddingbe/domain/comment/service/CommentUpdateService.java
@@ -1,7 +1,7 @@
 package com.example.puddingbe.domain.comment.service;
 
 import com.example.puddingbe.domain.comment.entity.Comment;
-import com.example.puddingbe.domain.comment.entity.dto.CommentRequestDTO;
+import com.example.puddingbe.domain.comment.presentation.dto.CommentRequestDTO;
 import com.example.puddingbe.domain.comment.entity.repository.CommentRepository;
 import com.example.puddingbe.global.detail.UserFacade;
 import lombok.RequiredArgsConstructor;
