6a4f9adc12a390d6a97d1c985d84a7b30e15861a
김지윤 <dpdupplexx9@gmail.com>
2025-10-24 19:47:47 +0900

    feat : 랭킹 브랜치 재 생성

---
+++ src/main/java/com/example/puddingbe/domain/ranking/domain/repository/RankingRepository.java
@@ -0,0 +1,8 @@
+package com.example.puddingbe.domain.ranking.domain.repository;
+
+import com.example.puddingbe.domain.ranking.domain.Ranking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RankingRepository extends JpaRepository<Ranking, Long> {
+
+}
