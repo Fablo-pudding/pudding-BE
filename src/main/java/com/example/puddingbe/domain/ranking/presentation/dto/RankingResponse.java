0ac0c39393f98313e8c4489703bf6f6826de3e98
김지윤 <dpdupplexx9@gmail.com>
2025-10-26 22:18:49 +0900

    feat : 기본 코드 추가

--- src/main/java/com/example/puddingbe/domain/ranking/presentation/dto/RankingResponse.java
+++ src/main/java/com/example/puddingbe/domain/ranking/presentation/dto/RankingResponse.java
@@ -1,9 +1,20 @@
 package com.example.puddingbe.domain.ranking.presentation.dto;
 
+import com.example.puddingbe.domain.ranking.domain.Ranking;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class RankingResponse {
+    private int rank;
+    private long id;
+    private long userId;
+    private long totalPudding;
+
+    public RankingResponse(Ranking ranking) {
+        this.rank = getRank();
+        this.userId = getUserId();
+        this.totalPudding = getTotalPudding();
+    }
 }
--- src/main/java/com/example/puddingbe/domain/ranking/service/RankingService.java
+++ src/main/java/com/example/puddingbe/domain/ranking/service/RankingService.java
@@ -1,9 +1,25 @@
 package com.example.puddingbe.domain.ranking.service;
 
+import com.example.puddingbe.domain.ranking.domain.repository.RankingRepository;
+import com.example.puddingbe.domain.ranking.presentation.dto.RankingResponse;
+import com.example.puddingbe.global.detail.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RankingService {
+    private final RankingRepository rankingRepository;
+    private final UserFacade userFacade;
+
+    public List<RankingResponse> getAllRankings() {
+        return rankingRepository.findAllOrderByTotalPuddingDesc();
+    }
+
+    public RankingResponse getUserRanking(Long userId) {
+        return rankingRepository.findByUserId(userId)
+                .orElseThrow(()->new RuntimeException("유저를 찾을 수 없습니다"));
+    }
 }
