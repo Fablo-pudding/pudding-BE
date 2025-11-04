97dd497918ec5d017be4f1bc0c489a27c973e82b
iokshoi <iokshoi0915@dsm.hs.kr>
2025-11-04 23:10:48 +0900

    refactor
    수정

--- src/main/java/com/example/puddingbe/domain/timer/sevice/TimerReadService.java
+++ src/main/java/com/example/puddingbe/domain/timer/sevice/TimerReadService.java
@@ -17,7 +17,7 @@ public class TimerReadService {
         TimerEntity timerEntity = timerRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED));
 
-        timerRepository.delete(timerEntity); // 이미 값은 받아왔기 때문에 return 전에 db를 delete해주면 됩니다.
+        timerRepository.delete(timerEntity); // 이미 값은 받아왔기 때문에 return 전에 DB를 delete 해주면 됩니다.
 
         return TimerResponse.builder() // return 문은 완전히 끝나는 구문.
                 .totalTime(timerEntity.getTotalTime())
