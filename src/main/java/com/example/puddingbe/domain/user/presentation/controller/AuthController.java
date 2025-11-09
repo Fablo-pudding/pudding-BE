52f5a4062367207fd047d7a758a4450587caabc1
iokshoi <iokshoi0915@dsm.hs.kr>
2025-11-09 15:31:02 +0900

    feat
    Logout API 구성

--- src/main/java/com/example/puddingbe/domain/user/presentation/controller/AuthController.java
+++ src/main/java/com/example/puddingbe/domain/user/presentation/controller/AuthController.java
@@ -2,10 +2,13 @@ package com.example.puddingbe.domain.user.presentation.controller;
 
 
 import com.example.puddingbe.domain.user.presentation.dto.request.SignUpRequest;
+import com.example.puddingbe.domain.user.service.LogoutService;
 import com.example.puddingbe.domain.user.service.SignupService;
 import com.example.puddingbe.global.jwt.JwtToken;
 import com.example.puddingbe.domain.user.presentation.dto.request.LoginRequest;
 import com.example.puddingbe.domain.user.service.LoginService;
+import io.jsonwebtoken.Jwt;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,7 @@ public class AuthController {
 
     private final SignupService signupService;
     private final LoginService loginService;
+    private final LogoutService logoutService;
 
     @PostMapping("/signup")
     public ResponseEntity<HttpStatus> signup(@Validated @RequestBody SignUpRequest signUpRequest) { // Valid를 추가해야 dto의 조건들 적용가능
@@ -31,5 +35,11 @@ public class AuthController {
     public ResponseEntity<JwtToken> login(@Validated @RequestBody LoginRequest loginRequest) {
         JwtToken jwtToken = loginService.login(loginRequest.getName(), loginRequest.getPassword());
         return ResponseEntity.ok(jwtToken); // jwt 반환
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) { // 클라이언트의 요청중에 Authorization 헤더에있는 토큰을 추출 학기 위함
+        logoutService.logout(request);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/example/puddingbe/global/config/SecurityConfig.java
+++ src/main/java/com/example/puddingbe/global/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
 
                         //user API
                         .requestMatchers("/user/signup", "/user/login").permitAll()
+                        .requestMatchers("/user/logout").authenticated()
                         .requestMatchers("/user/info").authenticated()
 
                         //timer API
