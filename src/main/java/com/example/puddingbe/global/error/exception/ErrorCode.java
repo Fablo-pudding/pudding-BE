e5a90a6da396642b3879ecfe9ac89598aaade1e4
iokshoi <iokshoi0915@dsm.hs.kr>
2025-11-17 20:22:17 +0900

    feat
    오류 코드 초안 구성코드 재 커밋

--- src/main/java/com/example/puddingbe/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/puddingbe/global/error/exception/ErrorCode.java
@@ -15,10 +15,14 @@ public enum ErrorCode {
     Less_Pudding_Two_Exception(400,"2성 푸딩 갯수가 부족합니다"),
 
 //Notice
-    Only_Admin_Create_Notice_Exception(403,"관리자만 공지를 작성할 수 있습니다"),
-    Only_Admin_Delete_Notice_Exception(403,"관리자만 공지를 삭제할 수 있습니다"),
-    Only_Admin_Update_Notice_Exception(403,"관리자만 공지를 수정할 수 있습니다");
+    Only_Admin_Create_Notice_Exception(403,"관리자만 공지를 작성할 수 있습니다."),
+    Only_Admin_Delete_Notice_Exception(403,"관리자만 공지를 삭제할 수 있습니다."),
+    Only_Admin_Update_Notice_Exception(403,"관리자만 공지를 수정할 수 있습니다"),
 
+//user
+    DuplicateUserException(409, "중복된 사용자 이름입니다"),
+    UnauthorizedUserPasswordException(401, "비밀번호가 일치하지 않습니다"),
+    UnauthorizedUserNameException(401, "찾을 수 없는 사용자입니다");
     private final Integer status;
     private final String message;
 
