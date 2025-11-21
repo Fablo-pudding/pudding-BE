69c70436bfb6dcb279ece1846821e80e81058492
김지윤 <dpdupplexx9@gmail.com>
2025-11-21 22:22:25 +0900

    feat : 랭킹 전체조회, 내 랭킹 조회 기능 추가

--- src/main/java/com/example/puddingbe/domain/refrigerator/domain/repository/PuddingRepository.java
+++ src/main/java/com/example/puddingbe/domain/refrigerator/domain/repository/PuddingRepository.java
@@ -1,7 +1,14 @@
 package com.example.puddingbe.domain.refrigerator.domain.repository;
 import com.example.puddingbe.domain.refrigerator.domain.entity.Pudding;
+import com.example.puddingbe.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface PuddingRepository extends JpaRepository<Pudding, Long> {
     Pudding findByUserId(Long userId);
+    Pudding findByUser(User user);
+
+    @Query(value = "SELECT * FROM tbl_pudding ORDER BY pudding_3 DESC ", nativeQuery = true)
+    List<Pudding> findAllByOrderByPudding3Desc();
 }
