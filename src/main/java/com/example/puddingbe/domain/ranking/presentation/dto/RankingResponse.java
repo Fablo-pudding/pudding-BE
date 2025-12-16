a059bd588b438e3c35ebf778d04de1ad9237734f
김지윤 <jjunkim@dsm.hs.kr>
2025-12-16 19:52:40 +0900

    feat : response 프로필 이미지 추가

--- src/main/java/com/example/puddingbe/domain/ranking/presentation/dto/RankingResponse.java
+++ src/main/java/com/example/puddingbe/domain/ranking/presentation/dto/RankingResponse.java
@@ -10,12 +10,14 @@ public class RankingResponse {
     private int rank;
     private long userId;
     private String userName;
+    private String userProfileImageUrl;
     private long pudding3;
 
     public RankingResponse(Pudding pudding, int rank) {
         this.rank = rank;
         this.userId = pudding.getUser().getId();
         this.userName = pudding.getUser().getName();
+        this.userProfileImageUrl = pudding.getUser().getProfileImageUrl();
         this.pudding3 = pudding.getPudding_3();
     }
 }
