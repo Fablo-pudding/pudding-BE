fea72814f49f379a0a3ff65105423bec3ebfee4f
rudwk <tlsruwk12345@gmail.com>
2026-01-11 16:36:36 +0900

    refactor: 디자인에 맞게 postListResponse 수정

--- src/main/java/com/example/puddingbe/domain/feed/presentation/dto/PostListResponseDTO.java
+++ src/main/java/com/example/puddingbe/domain/feed/presentation/dto/PostListResponseDTO.java
@@ -8,8 +8,6 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class PostListResponseDTO {
-    Long postId;
-    Long userId;
     String profileImageUrl;
     String name;
     String title;
@@ -17,8 +15,6 @@ public class PostListResponseDTO {
     Long commentCount;
 
     public PostListResponseDTO(Post post) {
-        this.postId = post.getPostId();
-        this.userId = post.getUser().getId();
         this.title = post.getTitle();
         this.createdAt = post.getCreatedAt().toString();
         this.name = post.getUser().getName();
