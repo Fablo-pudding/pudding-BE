b1d6043262cc0b59a67d9bd0d7baa45d70028e67
Kanghee <kh63167574@gmail.com>
2026-01-11 16:20:27 +0900

    충돌해결

--- src/main/java/com/example/puddingbe/domain/notice/presentation/dto/NoticeCheckResponse.java
+++ src/main/java/com/example/puddingbe/domain/notice/presentation/dto/NoticeCheckResponse.java
@@ -18,14 +18,7 @@ public class NoticeCheckResponse {
 
     public NoticeCheckResponse(NoticeUser noticeUser) {
         this.id = noticeUser.getId();
-<<<<<<< HEAD
-<<<<<<< HEAD
         this.profileImageUrl = noticeUser.getUser().getProfileImageUrl();
-=======
->>>>>>> 89443cb (refacotring 공지사항 작성자추가)
-=======
-        this.profileImageUrl = noticeUser.getUser().getProfileImageUrl();
->>>>>>> 123594a (refactoring : notice 상제조회 수정 profile url 가져오기 feed 전체조회 수정)
         this.registerEmployeeNo = noticeUser.getRegisterEmployeeNo();
         this.title = noticeUser.getTitle();
         this.content = noticeUser.getContent();
