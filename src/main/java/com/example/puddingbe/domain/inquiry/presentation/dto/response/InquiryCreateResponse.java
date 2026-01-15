536e323428bafcb0fc87308887f89a0e93a93f54
김지윤 <jjunkim@dsm.hs.kr>
2026-01-15 11:24:24 +0900

    refactor: 문의 생성 후 id 반환

---
+++ src/main/java/com/example/puddingbe/domain/inquiry/presentation/dto/response/InquiryCreateResponse.java
@@ -0,0 +1,10 @@
+package com.example.puddingbe.domain.inquiry.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InquiryCreateResponse {
+    private Long id;
+}
